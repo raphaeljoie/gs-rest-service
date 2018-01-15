de637d6cc374a1b65f2e4447040ad21bece3ed04
Raphaël Joie <raphael.joie@webiteasy.be>
2018-01-15 11:38:14 +0100

    Add host_address in /greeting endpoint

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -2,19 +2,26 @@ package hello;
 
 public class Greeting {
 
+    /**
+     * Id of request, unique inside a run
+     */
     private final long id;
-    private final String content;
 
-    public Greeting(long id, String content) {
+    /**
+     *
+     */
+    private final String host_address;
+
+    public Greeting(long id, String host_address) {
         this.id = id;
-        this.content = content;
+        this.host_address = host_address;
     }
 
     public long getId() {
         return id;
     }
 
-    public String getContent() {
-        return content;
+    public String getHost_address() {
+        return host_address;
     }
 }
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,9 +1,12 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class GreetingController {
@@ -11,9 +14,12 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @SuppressWarnings("unused")
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+                             HttpServletRequest request) {
+
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                request.getLocalAddr());
     }
 }
