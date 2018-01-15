47c8884ec4708208508eb6e0b2f67c609ed02cfc
Raphaël Joie <raphael.joie@webiteasy.be>
2018-01-15 11:41:10 +0100

    add memory informations in GET /greetings

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -12,9 +12,39 @@ public class Greeting {
      */
     private final String host_address;
 
+    /**
+     * the maximum amount of memory that the virtual machine will attempt to use, measured in bytes
+     * {@link Runtime#maxMemory()}
+     */
+    private final long memory_max;
+
+    /**
+     * the total amount of memory currently available for current
+     * and future objects, measured in bytes. {@link Runtime#totalMemory()}
+     */
+    private final long memory_allocated;
+
+    /**
+     * an approximation to the total amount of memory currently
+     * available for future allocated objects, measured in bytes. {@link Runtime#freeMemory()}
+     */
+    private final long memory_free;
+
+    /**
+     * TODO
+     */
+    private final long memory_free_total;
+
     public Greeting(long id, String host_address) {
         this.id = id;
         this.host_address = host_address;
+
+        Runtime runtime = Runtime.getRuntime();
+
+        this.memory_max = runtime.maxMemory();
+        this.memory_allocated = runtime.totalMemory();
+        this.memory_free = runtime.freeMemory();
+        this.memory_free_total = memory_free + (memory_max - memory_allocated);
     }
 
     public long getId() {
@@ -23,5 +53,21 @@ public class Greeting {
 
     public String getHost_address() {
         return host_address;
+    }
+
+    public long getMemory_max() {
+        return memory_max;
+    }
+
+    public long getMemory_allocated() {
+        return memory_allocated;
+    }
+
+    public long getMemory_free() {
+        return memory_free;
+    }
+
+    public long getMemory_free_total() {
+        return memory_free_total;
     }
 }
